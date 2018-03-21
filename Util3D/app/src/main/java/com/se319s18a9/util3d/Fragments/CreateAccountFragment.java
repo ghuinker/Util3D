ghuinker/Util3D

commit 6140904cd9100080605d2a1cd3444b44ff81df24

Added to create account and stores info in database

Util3D/app/src/main/java/com/se319s18a9/util3d/Fragments/CreateAccountFragment.java
@@ -1,6 +1,5 @@
 package com.se319s18a9.util3d.Fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,64 +11,49 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.se319s18a9.util3d.database.UserInfo;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.se319s18a9.util3d.R;
 import com.se319s18a9.util3d.backend.User;
-import com.google.firebase.database.FirebaseDatabase;
-import com.se319s18a9.util3d.database.UserInfo;
 
 public class CreateAccountFragment extends Fragment implements View.OnClickListener {
 
-    OnAccountCreatedListener mCallback;
+    private EditText emailEditText;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private EditText repeatPasswordEditText;
+    private EditText fullName;
+    private EditText companyName;
+    private EditText phoneNumber;
+    private EditText occupation;
 
     Button createButton;
     Button cancelButton;
 
-    private EditText fullName;
-    private EditText companyName;
-    private EditText occupation;
-    private EditText phoneNumber;
-
     private DatabaseReference databaseReference;
     private FirebaseAuth mAuth;
-
-    public interface OnAccountCreatedListener {
-        void onAccountCreated(String username, String password);
-    }
 
     public CreateAccountFragment() {
         // Empty constructor
     }
 
+
     public void saveUser() {
 
         databaseReference = FirebaseDatabase.getInstance().getReference();
 
-//        String name = fullName.getText().toString().trim();
-//        String occ = occupation.getText().toString().trim();
-//        String company = companyName.getText().toString().trim();
-//        String phone = phoneNumber.getText().toString().trim();
-//
-//        UserInfo userInfo = new UserInfo(company, name, occ, phone);
+        String name = fullName.getText().toString().trim();
+        String occ = occupation.getText().toString().trim();
+        String company = companyName.getText().toString().trim();
+        String phone = phoneNumber.getText().toString().trim();
 
-        FirebaseUser user = mAuth.getCurrentUser();
+        UserInfo userInfo = new UserInfo(company, name, occ, phone);
 
-        //databaseReference.child(user.getUid()).setValue(userInfo);
+        databaseReference.child(User.getInstance().getUserID()).setValue(userInfo);
 
         //Toast.makeText(this, "Information Updated",Toast.LENGTH_LONG).show();
 
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-//        try {
-//            mCallback = (OnAccountCreatedListener) context;
-//        } catch (ClassCastException e) {
-//            throw new ClassCastException(context.toString() + " must implement OnAccountCreatedListener");
-//        }
     }
 
     @Override
@@ -80,7 +64,20 @@ public class CreateAccountFragment extends Fragment implements View.OnClickListe
 
         View v = inflater.inflate(R.layout.fragment_createaccount, container, false);
 
+        getActivity().setTitle("Create Account");
+
+        mAuth = FirebaseAuth.getInstance();
+
         // Initialize EditTexts and Buttons
+
+        emailEditText = v.findViewById(R.id.fragment_createAccount_editText_email);
+        usernameEditText = v.findViewById(R.id.fragment_createAccount_editText_username);
+        passwordEditText = v.findViewById(R.id.fragment_createAccount_editText_password);
+        repeatPasswordEditText = v.findViewById(R.id.fragment_createAccount_editText_repeatPassword);
+        fullName = v.findViewById(R.id.fragment_createAccount_editText_fullName);
+        companyName = v.findViewById(R.id.fragment_createAccount_editText_companyName);
+        occupation = v.findViewById(R.id.fragment_createAccount_editText_companyName);
+        phoneNumber = v.findViewById(R.id.fragment_createAccount_editText_phoneNumber);
 
         createButton = v.findViewById(R.id.fragment_createAccount_button_create);
         createButton.setOnClickListener(this);
@@ -95,33 +92,24 @@ public class CreateAccountFragment extends Fragment implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.fragment_createAccount_button_create:
-                if(((EditText) this.getView().findViewById(R.id.fragment_createAccount_editText_password)).getText().toString().equals(
-                        ((EditText) this.getView().findViewById(R.id.fragment_createAccount_editText_repeatPassword)).getText().toString())){
-                    try{
-                        User.getInstance().createAccount(((EditText) this.getView().findViewById(R.id.fragment_createAccount_editText_email)).getText().toString(), ((EditText) this.getView().findViewById(R.id.fragment_createAccount_editText_password)).getText().toString());
-                        try{
-                            if(((EditText) this.getView().findViewById(R.id.fragment_createAccount_editText_username)).getText().toString()!=null&&
-                                    !((EditText) this.getView().findViewById(R.id.fragment_createAccount_editText_username)).getText().toString().isEmpty()) {
-                                User.getInstance().changeDisplayName(((EditText) this.getView().findViewById(R.id.fragment_createAccount_editText_username)).getText().toString());
-                            }
-                        } catch(Exception e){
-                            Toast.makeText(this.getContext(), R.string.s_fragment_createAccount_errorMessage_usernameNotSet, Toast.LENGTH_SHORT).show();
+                if(getEditTextValue(passwordEditText).equals(getEditTextValue(repeatPasswordEditText))) {
+                    try {
+                        User.getInstance().createAccount(getEditTextValue(emailEditText), getEditTextValue(passwordEditText));
+                        saveUser();
+
+                        if(!getEditTextValue(usernameEditText).isEmpty()) {
+                            User.getInstance().changeDisplayName(getEditTextValue(usernameEditText));
                         }
-                        //TODO: verify this is correct way to do this
-                        getActivity().getSupportFragmentManager().popBackStackImmediate();
-                    } catch(Exception e){
+                    } catch(Exception e) {
                         Toast.makeText(this.getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
-                }
-                else {
+
+                    getActivity().getSupportFragmentManager().popBackStackImmediate();
+                } else {
                     Toast.makeText(v.getContext(), R.string.s_fragment_createAccount_errorMessage_PasswordsNotMatching, Toast.LENGTH_SHORT).show();
                 }
-
-
                 break;
             case R.id.fragment_createAccount_button_cancel:
-                // TODO: Discard credentials and return to LoginFragment
-                //Toast.makeText(this.getContext(), R.string.s_fragment_createAccount_debug_cancel, Toast.LENGTH_SHORT).show(); // DEBUG
                 getActivity().onBackPressed();
                 break;
         }

Util3D/app/src/main/java/com/se319s18a9/util3d/backend/User.java
@@ -163,6 +163,10 @@ public class User {
         }
     }
 
+    public String getUserID(){
+       return mAuth.getCurrentUser().getUid();
+    }
+
     /**
      * Change username. Returns a boolean since the firebase method does not throw any exceptions.
      * @param displayName

Util3D/app/src/main/res/layout/fragment_createaccount.xml
@@ -4,9 +4,73 @@
     android:layout_height="match_parent"
     android:layout_width="match_parent"
     android:orientation="vertical"
-    android:paddingBottom="16dp"
-    android:paddingTop="16dp"
+    android:paddingBottom="3dp"
+    android:paddingTop="3dp"
     tools:context="com.se319s18a9.util3d.Fragments.LoginFragment">
+
+    <android.support.design.widget.TextInputLayout
+        android:layout_height="wrap_content"
+        android:layout_width="match_parent">
+
+    <android.support.design.widget.TextInputEditText
+        android:id="@+id/fragment_createAccount_editText_fullName"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:layout_marginEnd="16dp"
+        android:layout_marginStart="16dp"
+        android:hint="@string/s_fragment_createAccount_editText_hint_fullName"
+        android:inputType="textPersonName"
+        android:paddingEnd="8dp"
+        android:paddingStart="8dp" />
+
+    <android.support.design.widget.TextInputLayout
+        android:layout_height="wrap_content"
+        android:layout_width="match_parent">
+
+    <android.support.design.widget.TextInputEditText
+        android:id="@+id/fragment_createAccount_editText_companyName"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:layout_marginEnd="16dp"
+        android:layout_marginStart="16dp"
+        android:hint="@string/s_fragment_createAccount_editText_hint_companyName"
+        android:inputType="text"
+        android:paddingEnd="8dp"
+        android:paddingStart="8dp" />
+    </android.support.design.widget.TextInputLayout>
+
+    <android.support.design.widget.TextInputLayout
+        android:layout_height="wrap_content"
+        android:layout_width="match_parent">
+
+        <android.support.design.widget.TextInputEditText
+            android:id="@+id/fragment_createAccount_editText_occupation"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:layout_marginEnd="16dp"
+            android:layout_marginStart="16dp"
+            android:hint="@string/s_fragment_createAccount_editText_hint_occupation"
+            android:inputType="text"
+            android:paddingEnd="8dp"
+            android:paddingStart="8dp" />
+    </android.support.design.widget.TextInputLayout>
+
+    <android.support.design.widget.TextInputLayout
+        android:layout_height="wrap_content"
+        android:layout_width="match_parent">
+
+        <android.support.design.widget.TextInputEditText
+            android:id="@+id/fragment_createAccount_editText_phoneNumber"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:layout_marginEnd="16dp"
+            android:layout_marginStart="16dp"
+            android:hint="@string/s_fragment_createAccount_editText_hint_phoneNumber"
+            android:inputType="phone"
+            android:paddingEnd="8dp"
+            android:paddingStart="8dp" />
+    </android.support.design.widget.TextInputLayout>
+
 
     <android.support.design.widget.TextInputLayout
         android:layout_height="wrap_content"
@@ -76,6 +140,8 @@
 
     </android.support.design.widget.TextInputLayout>
 
+    </android.support.design.widget.TextInputLayout>
+
     <LinearLayout
         android:gravity="center"
         android:layout_height="wrap_content"
@@ -107,7 +173,6 @@
             android:layout_width="0dp"
             android:text="@string/s_fragment_createAccount_button_cancel"
             tools:ignore="ButtonStyle" />
+        </LinearLayout>
 
     </LinearLayout>
-
-</LinearLayout>

Util3D/app/src/main/res/values/strings.xml
@@ -26,6 +26,10 @@
     <!-- CreateAccountFragment Strings -->
     <string name="s_fragment_createAccount_title">Create New Account</string>
     <string name="s_fragment_createAccount_editText_hint_username">Username</string>
+    <string name="s_fragment_createAccount_editText_hint_fullName">Name</string>
+    <string name="s_fragment_createAccount_editText_hint_companyName">Company Name</string>
+    <string name="s_fragment_createAccount_editText_hint_occupation">Occupation</string>
+    <string name="s_fragment_createAccount_editText_hint_phoneNumber">Phone Number</string>
     <string name="s_fragment_createAccount_editText_hint_email">Email</string>
     <string name="s_fragment_createAccount_editText_hint_password">Password</string>
     <string name="s_fragment_createAccount_editText_hint_repeatPassword">Repeat Password</string>

commit 2e37666444443c7ebf4b7151c69c174e17cd213e

Fix create account backout regression introduced in commit 6140904cd9100080605d2a1cd3444b44ff81df24

Util3D/app/src/main/java/com/se319s18a9/util3d/Fragments/CreateAccountFragment.java
@@ -96,15 +96,13 @@ public class CreateAccountFragment extends Fragment implements View.OnClickListe
                     try {
                         User.getInstance().createAccount(getEditTextValue(emailEditText), getEditTextValue(passwordEditText));
                         saveUser();
-
                         if(!getEditTextValue(usernameEditText).isEmpty()) {
                             User.getInstance().changeDisplayName(getEditTextValue(usernameEditText));
                         }
+                        getActivity().getSupportFragmentManager().popBackStackImmediate();
                     } catch(Exception e) {
                         Toast.makeText(this.getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
-
-                    getActivity().getSupportFragmentManager().popBackStackImmediate();
                 } else {
                     Toast.makeText(v.getContext(), R.string.s_fragment_createAccount_errorMessage_PasswordsNotMatching, Toast.LENGTH_SHORT).show();
                 }