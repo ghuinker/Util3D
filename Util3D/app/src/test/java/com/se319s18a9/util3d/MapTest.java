123ef043b6d4a58731d1a8b93e57afe72a9f9302
Malcolm Johnson <maj401@iastate.edu>
2018-03-25 17:56:39 -0500

    Local Cleanup- Removed A9 folder

---
+++ Util3D/app/src/test/java/com/se319s18a9/util3d/MapTest.java
@@ -0,0 +1,25 @@
+package com.se319s18a9.util3d;
+
+import com.se319s18a9.util3d.backend.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by malco on 3/22/2018.
+ */
+
+public class MapTest {
+    Map testMap;
+
+    @Before
+    public void setup() {
+        testMap = new Map();
+    }
+
+    @Test
+    public void addLineTest() {
+
+    }
+
+}
